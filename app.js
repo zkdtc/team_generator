7c5b7b7c7db63888a737876320ef3b140dbbaacc
zhangkai <zkdtckk@gmail.com>
2020-06-15 07:12:16 -0700

    prompts work

---
+++ app.js
@@ -0,0 +1,154 @@
+var inquirer = require("inquirer");
+
+var fs = require('fs');
+
+enterNew();
+var data_all=[];
+
+function enterNew() {
+
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "Welcome to the TEAM generator! Please select your role:",
+            choices: [
+                'Manager', 'Engineer', 'Intern'
+            ]
+        }
+    ]).then(async (data) =>{
+            console.log(data.role);
+            switch (data.role) {
+                case 'Manager':
+                    enterManager();
+                    break;
+                case 'Engineer':
+                    enterEngineer();
+                    break;
+                case 'Intern':
+                    enterIntern();
+                    break;
+            }
+
+        })
+
+}
+
+function enterManager() {
+    
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter your name:",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter your id:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email:",
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "Please enter your office number:",
+        },
+        {
+            type: 'confirm',
+            name: 'askAgain',
+            message: 'Want to enter another employee (just hit enter for YES)?',
+            default: true
+          }
+
+    ]).then(async (data) =>{
+        data_all.push(data);
+        console.log(data_all);
+        if (data.askAgain){
+            enterNew();
+        }
+        
+    })
+
+}
+
+function enterEngineer() {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter your name:",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter your id:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email:",
+        },
+        {
+            type: "input",
+            name: "githubusername",
+            message: "Please enter your Github Username:",
+        },
+        {
+            type: 'confirm',
+            name: 'askAgain',
+            message: 'Want to enter another employee (just hit enter for YES)?',
+            default: true
+          }
+
+    ]).then(async (data) => {
+        data_all.push(data);
+        console.log(data_all);
+        if (data.askAgain){
+            enterNew();
+        }
+    })
+}
+
+function enterIntern() {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter your name:",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter your id:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email:",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Please enter your school:",
+        },
+        {
+            type: 'confirm',
+            name: 'askAgain',
+            message: 'Want to enter another employee (just hit enter for YES)?',
+            default: true
+          }
+
+    ]).then(async (data) => {
+        data_all.push(data);
+        console.log(data_all);
+        if (data.askAgain){
+            enterNew();
+        }
+    })
+}
